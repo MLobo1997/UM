1f3359c0614777fb990825995b9aa444f4dd7be6
MLobo1997 <miguelobo1997@gmail.com>
2017-02-22 14:05:10 +0000

    kaboom

--- 2o_ano/2oSemestre/SO/guiao1/nl-v1.c
+++ 2o_ano/2oSemestre/SO/guiao1/nl-v1.c
@@ -7,23 +7,25 @@
 
 ssize_t readln(int fildes, char *buf, size_t maxlen){
 
-    int i, n=1;
-    char ch='\0';
+    int i, n;
+    char ch;
 
-    for(i=0; i < maxlen && ch !='\n' && n == 1; i++){
+    i=0;
+    do{
         n = read(fildes, &ch, 1);
         buf[i]=ch;
-    }
+        if(n==1) i++;
+    }while(i < maxlen && ch !='\n' && ch != '\0' && ch != EOF && n == 1);
 
     return i;
 }
 
 int main (int argc, char **argv){
-    int count=1, line=1;
+    int count=-1, line=1, fildes;
     char print[1000], l[100];
 
     if(argc == 1){
-        while(count > 0){
+        do{
             count = readln(0, print, 1000);
 
             sprintf(l, "%d\t", line);
@@ -31,8 +33,22 @@ int main (int argc, char **argv){
             write(1, print, count);
 
             line++;
-        }
+        }while(count > 0);
     }
 
+    if(argc == 2){
+        fildes = open(argv[1], O_RDONLY);
+        if(fildes < 0) perror("FILE NOT FOUND\n"), exit(-1);
+        do{
+            count = readln(fildes, print, 1000);
+
+            if(count > 0){sprintf(l, "%d\t", line);
+                write(1, l, strlen(l));
+                write(1, print, count);
+            }
+
+            line++;
+        }while(count > 0);
+    }
     exit(count);
 }
